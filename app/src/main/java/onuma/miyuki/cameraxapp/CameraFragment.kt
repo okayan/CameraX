41d9750dbd4dfd981abfdb1593b03205a6aeee91
miyuki onuma <miyuki.onuma@iridge.jp>
2019-12-25 16:15:54 +0900

    fix: initial commit

--- app/src/main/java/onuma/miyuki/cameraxapp/CameraFragment.kt
+++ app/src/main/java/onuma/miyuki/cameraxapp/CameraFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.LensFacing
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject
+import kotlinx.android.synthetic.main.fragment_camera.overlay
 import kotlinx.android.synthetic.main.fragment_camera.previewView
+import java.util.concurrent.Executors
 
-class CameraFragment: Fragment() {
+class CameraFragment : Fragment(), LifecycleOwner {
 
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
@@ -53,7 +58,50 @@ class CameraFragment: Fragment() {
             .requireLensFacing(LensFacing.BACK)
             .build()
 
-        cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.BackpressureStrategy.KEEP_ONLY_LATEST)
+            .build()
+
+        imageAnalysis.setAnalyzer(
+            Executors.newSingleThreadExecutor(),
+            ObjectDetectionAnalyzer {
+                overlay?.post {
+                    updateOverlay(it)
+                }
+            }
+        )
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis)
     }
 
+    private fun updateOverlay(detectedObjects: DetectedObjects) {
+
+        if (detectedObjects.objects.isEmpty()) {
+            overlay?.set(emptyList())
+            return
+        }
+
+        overlay.setSize(detectedObjects.imageWidth, detectedObjects.imageHeight)
+
+        val list = mutableListOf<BoxData>()
+
+        for (obj in detectedObjects.objects) {
+
+            val box = obj.boundingBox
+
+            val name = "${categoryNames[obj.classificationCategory]}"
+
+            val confidence =
+                if (obj.classificationCategory != FirebaseVisionObject.CATEGORY_UNKNOWN) {
+                    val confidence: Int = obj.classificationConfidence!!.times(100).toInt()
+                    " $confidence%"
+                } else {
+                    ""
+                }
+
+            list.add(BoxData("$name$confidence", box))
+        }
+
+        overlay.set(list)
+    }
 }
--- app/src/main/java/onuma/miyuki/cameraxapp/MyApplication.kt
+++ app/src/main/java/onuma/miyuki/cameraxapp/MyApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
 
-class MyApplication: Application(), CameraXConfig.Provider {
+class MyApplication : Application(), CameraXConfig.Provider {
     override fun getCameraXConfig(): CameraXConfig {
         return Camera2Config.defaultConfig(this)
     }
